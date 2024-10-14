2c9c836d49b5f6747be5fb64ba04aa6352d73779
= <=>
2024-10-14 08:30:01 +0500

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
 import express from "express";
-import createHttpError from "http-errors";
+// import createHttpError from "http-errors";
 import globleErrorhandler from "./middlewares/globalErrorhandler";
+import userRouter from "./users/userRouter";
 
 const app = express();
 
 // Routes
 app.get("/", (req, res) => {
-  const error = createHttpError(400, "Something went wrong");
-  throw error;
-
+  //   const error = createHttpError(400, "Something went wrong");
+  //   throw error;
   res.json({ message: "Welcome to E APIs" });
 });
 
+app.use("/api/users/", userRouter);
 app.use(globleErrorhandler);
 
 export default app;
