4f00b27eec3eddd4581db7f1e4e5179a2346c444
= <=>
2024-10-15 08:14:57 +0500

    add error handling

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -14,28 +15,43 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   // Check existing email in Mongodb
-  const user = await userModel.findOne({ email: email });
-  if (user) {
-    const error = createHttpError(400, "User already exist with this email.");
-    return next(error);
+  try {
+    const user = await userModel.findOne({ email: email });
+    if (user) {
+      const error = createHttpError(400, "User already exist with this email.");
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(500, "Error while getting user" + error));
   }
 
   // hash password
   const hashedPassword = await bcrypt.hash(password, 10);
 
   // store inside db
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
+  let newUser: User;
+  try {
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while Creating user" + error));
+  }
 
-  // Add JWT Token
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-    algorithm: "HS256",
-  });
-  res.json({ accessToken: token });
+  try {
+    // Add JWT Token
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+    res.json({ accessToken: token });
+  } catch (error) {
+    return next(
+      createHttpError(500, "Error while Signing the JWT token" + error)
+    );
+  }
 };
 
 export { createUser };
