bc335e733fa5c7e1f7405ef390f1c6e123bb97a5
= <=>
2024-10-15 07:41:32 +0500

    implement hash password

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -8,6 +10,16 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "All fields are required");
     return next(error);
   }
+
+  // Check existing email in Mongodb
+  const user = await userModel.findOne({ email: email });
+  if (user) {
+    const error = createHttpError(400, "User already exist with this email.");
+    return next(error);
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   res.json({ message: "registered user" });
 };
