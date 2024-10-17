da6454fefe85d0b1c49bc126533703147f6c3ab0
= <=>
2024-10-17 22:25:17 +0500

    Setup Multer multipartform data

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,7 +1,22 @@
 import express from "express";
 import { createBook } from "./bookController";
+import multer from "multer";
+import path from "node:path";
 const bookRouter = express.Router();
 
-bookRouter.post("/", createBook);
+// file store local  ->
+const upload = multer({
+  dest: path.resolve(__dirname, "../../public/data/uploads"),
+  limits: { fileSize: 3e7 }, // byte me data store kry ga.    // 30MB  <=  30 * 1024 * 1024
+});
+
+bookRouter.post(
+  "/",
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  createBook
+);
 
 export default bookRouter;
