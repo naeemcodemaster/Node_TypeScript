cfaf38be0ea51a195030c6d57b6aec4a279d9ff1
= <=>
2024-10-19 15:11:27 +0500

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import cors from "cors";
 // import createHttpError from "http-errors";
 import globleErrorhandler from "./middlewares/globalErrorhandler";
 import userRouter from "./users/userRouter";
 import bookRouter from "./book/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 app.use(express.json());
 // Routes
 app.get("/", (req, res) => {
--- src/config/config.ts
+++ src/config/config.ts
@@ -10,6 +10,7 @@ const _config = {
   cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
   cloudinaryAPIKey: process.env.CLOUDINARY_API_KEY,
   cloudinaryAPISecret: process.env.CLOUDINARY_API_SECRET,
+  frontendDomain: process.env.FRONTEND_DOMAIN,
 };
 
 // port overwrite nhi hoga dosri file me freeze kya hua a.
