2156b2681ee00231c0df1f7185711e4d475a522a
= <=>
2024-10-27 21:17:53 +0500

    allow all cors

--- src/app.ts
+++ src/app.ts
@@ -4,23 +4,25 @@ import cors from "cors";
 import globleErrorhandler from "./middlewares/globalErrorhandler";
 import userRouter from "./users/userRouter";
 import bookRouter from "./book/bookRouter";
-import { config } from "./config/config";
+// import { config } from "./config/config";
 
 const app = express();
 // CORS configuration with custom function to handle multiple allowed origins
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [config.frontendDomain, config.reactApp];
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       const allowedOrigins = [config.frontendDomain, config.reactApp];
+//       // Allow requests with no origin (like mobile apps or curl requests)
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
 app.use(express.json());
 // Routes
 app.get("/", (req, res) => {
